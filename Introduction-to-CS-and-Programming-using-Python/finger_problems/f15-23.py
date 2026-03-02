dc8225af8354fb1e79c93ce25e635f6412933051
fabirez <skyc4ff3ine@virgilio.it>
2026-03-02 18:40:57 +0100

    Added sortin-algo and complexity classes

--- Introduction-to-CS-and-Programming-using-Python/finger_problems/f15-23.py
+++ Introduction-to-CS-and-Programming-using-Python/finger_problems/f15-23.py
@@ -251,3 +251,52 @@ removed = queue.remove()
 assert removed is None, f"Test Failed: Expected removed element to be None, but got {removed}."
 print("Assert for queue passed");
 print("=======================================\n")
+
+
+###################################
+### Finger Exercise Lecture 23
+###################################
+
+# Question 1: Choose the worst-case asymptotic order of growth (upper and lower bound)
+## for the following function. Assume n = a. 
+
+def running_product(a):
+    """ a is an int """
+    product = 1
+    for i in range(5,a+5):
+        product *= i
+        if product == a:
+            return True
+    return False
+
+# Answer 1: Theta(n)
+
+# Question 2: Choose the worst-case asymptotic order of growth (upper and lower bound)
+## for the following function. Assume n = len(L).
+
+def tricky_f(L, L2):
+    """ L and L2 are lists of equal length """
+    inL = False
+    for e1 in L:
+        if e1 in L2:
+            inL = True
+    inL2 = False
+    for e2 in L2:
+        if e2 in L:
+            inL2 = True
+    return inL and inL2
+
+# Answer 2: Theta(n^2)
+
+# Question 3: Choose the worst-case asymptotic order of growth (upper and lower bound)
+## for the following function.
+
+def sum_f(n):
+    """ n > 0 """
+    answer = 0
+    while n > 0:
+        answer += n%10
+        n = int(n/10)
+    return answer
+
+# Answer 3: Theta(log n)
