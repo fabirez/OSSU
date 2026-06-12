d7610a22cbc62abf10f3dfef4839b4d32ddcad44
fabirez <skyc4ff3ine@virgilio.it>
2026-06-12 20:54:42 +0200

    n-queens

--- Systematic-Program-Design/15-Search/nqueens-starter.rkt
+++ Systematic-Program-Design/15-Search/nqueens-starter.rkt
@@ -195,52 +195,6 @@
 (define ALL_2 (list (make-pos 3 1) (make-pos 2 3) (make-pos 3 3)))
 (define ALL_3 (list (make-pos 3 1)))
 
-;; Board -> (listof Pos)
-;; from the consumed board, produce all the available position for a queen
-
-;; (check-expect (available-lop B0) ALL)
-;; (check-expect (available-lop B0_1) ALL_1)
-;; (check-expect (available-lop B0_2) ALL_2)
-;; (check-expect (available-lop B0_3) ALL_3)
-;;
-;; (define (available-lop b) empty) ; stub
-;;
-;; (define (available-lop b)
-;; (cond 
-;; 	[(empty? b) empty]
-;; 	[else
-;; 		(... (first b)
-;; 			(available-lop (rest b)))
-;; 	]))
-
-
-;; !!! 
-;; Board -> Number
-;; consuma a board and produce a number that rapresetent the first available col of the board.
-;; ASSUME: The Board is not solved.
-
-;; (check-expect (available-col   B0) 0) 
-;; (check-expect (available-col B0_1) 1) 
-;; (check-expect (available-col B0_2) 2) 
-;; (check-expect (available-col B0_3) 3) 
-
-;; (define (available-col b) 0) ;stub
-
-#; (define (available-col b)
-	(local [
-					(define (queen? c) (not (empty? (cell-v c)))) ;; Cell -> Boolean
-					(define loq (filter queen? b))								;; (listof Queen)
-					]
-	(if (empty? loq) 
-		0 
-		(+ (pos-x (cell-pos (first loq))) 1)))) 
-
-
-;; Board -> (listof Board)
-;; produce the next variation of boards based on the first availble column
-;; ASSUME: the board is not solved
-;; !!!
-
 #; (check-expect (next-boards B0) 
 							(list
 									(list
@@ -274,24 +228,7 @@
 (define (next-boards b) empty)
 ;; insert-queen is based on the list of position available of the find-availbe-lcol output
 
-#; (define (next-boards b)
-	(insert-queen (find-available-col b)))
-
-;; Board -> Number
-;; consume a board and produce the first index of the available columns
-
-(check-expect (find-available-col B0) 0)
-(check-expect (find-available-col B0_1) 1)
-(check-expect (find-available-col B0_2) 2)
-(check-expect (find-available-col B0_3) 3)
-
-;; (define (find-available-col b) 0)  ;; stub
-
-(define (find-available-col b)
-	(if (false? (cell-v (first b)))
-		(find-cell b (first b) 0)
-		(find-available-col (rest b))))
-
+#; (define (next-boards b) (insert-queen (find-available-col b)))
 
 ;; (listof Cell) Cell Number -> Number | false
 ;; consume a list, the value to be find and the initial index produce the current index of a value in a list or false if the value doesn't exist.
@@ -301,7 +238,7 @@
 (check-expect (find-cell (list (make-cell (make-pos 0 0) E) (make-cell (make-pos 1 0) E) (make-cell (make-pos 2 0) E)) (make-cell (make-pos 0 0) E) 0) 0)
 (check-expect (find-cell (list (make-cell (make-pos 0 0) E) (make-cell (make-pos 1 0) E) (make-cell (make-pos 2 0) E)) (make-cell (make-pos 2 0) E) 0) 2)
 
-;; (define (find loc c) 0) ; stub
+;; (define (find-cell loc c) 0) ; stub
 
 (define (find-cell loc c idx)
 	(cond 
@@ -322,10 +259,184 @@
 					idx
 					(find-cell (rest loc) c (add1 idx))))])) 
 
+;; Board -> (listof Pos)
+;; consume a board an produce the list of position that are available and valid 
+
+(check-expect (filter-board B0) ;; Board empty
+              (list   (make-pos 0 0)    (make-pos 1 0)  (make-pos 2 0)  (make-pos 3 0) 
+                      (make-pos 0 1)    (make-pos 1 1)  (make-pos 2 1)  (make-pos 3 1) 
+                      (make-pos 0 2)    (make-pos 1 2)  (make-pos 2 2)  (make-pos 3 2) 
+                      (make-pos 0 3)    (make-pos 1 3)  (make-pos 2 3)  (make-pos 3 3)))
+
+(check-expect (filter-board B0_1) ;; Board with 1 queen
+              (list   (make-pos 1 0)  (make-pos 3 0) 
+                      (make-pos 2 1)  (make-pos 3 1) 
+                      (make-pos 2 3)  (make-pos 3 3)))
+
+(check-expect (filter-board B0_2) ;; Board with 2 queen
+              (list   (make-pos 3 1) (make-pos 2 3)  (make-pos 3 3)))
+
+(check-expect (filter-board B0_3) ;; Board with 3 queen
+              (list (make-pos 3 1)))
+
+(check-expect (filter-board B0_S) ;; Board with 4 queen
+              empty)
+
+(define (filter-board b) empty) ; stub
+
+#; (define (filter-board b)  
+(local
+  [(define queens (only-queen b))] ;; Board -> (listof Queen)
+    (no-pos-duplicate (all-queens-pos queens (length b)))))
 
 
-;; Queen -> (listof pos) 
-;; consume a queen and produe the list of poistion that she occupy
+;; (listof Pos) (listof Pos) -> (listof Pos)
+;; consume
+;; the  first list of pos, that is all the available position in a empty baord NxN 
+;; the second list of pos, that is the position already occuped by queens inside the board.
+;; produce
+;; list of all available and vaild position not occupied by queen by filtering the first with the second
+;; (occoupied means that the queen cannot eat)
+;; [A]ll [P]os [4]x4
+(define AP4 (list                 
+                      (make-pos 0 0)    (make-pos 1 0)  (make-pos 2 0)  (make-pos 3 0) 
+                      (make-pos 0 1)    (make-pos 1 1)  (make-pos 2 1)  (make-pos 3 1) 
+                      (make-pos 0 2)    (make-pos 1 2)  (make-pos 2 2)  (make-pos 3 2) 
+                      (make-pos 0 3)    (make-pos 1 3)  (make-pos 2 3)  (make-pos 3 3)))
+
+
+(check-expect (only-valid-pos AP4 (queen-pos (make-cell (make-pos 0 2) Q) 4))
+              (list                 
+                      (make-pos 1 0)  (make-pos 3 0) 
+                      (make-pos 2 1)  (make-pos 3 1) 
+                      (make-pos 2 3)  (make-pos 3 3)))
+      
+
+;; (define (only-valid-pos lop0 lop1) empty) ; stub
+
+(define (only-valid-pos lop0 lop1)
+  (local
+    [(define (same-pos? p) (check-pos? p lop1))
+     (define (check-pos? p lop1)
+      (cond
+        [(empty? lop1) true]
+        [else
+          (if (and (= (pos-x p) (pos-x (first lop1))) (= (pos-y p) (pos-y (first lop1))))
+          false 
+          (check-pos? p (rest lop1)))]))]
+
+  (filter same-pos? lop0)))
+
+;; Board -> (listof Pos) 
+;; consume a board and produce a list of all the pos inside the board.
+
+(check-expect (get-lop B0)
+              (list   (make-pos 0 0)    (make-pos 1 0)  (make-pos 2 0)  (make-pos 3 0) 
+                      (make-pos 0 1)    (make-pos 1 1)  (make-pos 2 1)  (make-pos 3 1) 
+                      (make-pos 0 2)    (make-pos 1 2)  (make-pos 2 2)  (make-pos 3 2) 
+                      (make-pos 0 3)    (make-pos 1 3)  (make-pos 2 3)  (make-pos 3 3)))
+
+;; (define (get-lop b) empty)
+
+(define (get-lop b)
+  (local
+    [(define (only-pos c) (cell-pos c))]
+  (map only-pos b)))
+
+;; (listof Pos) -> (listof Pos)
+;; consume a list of positions and remove the duplicate pos inside of it.
+
+(check-expect (no-pos-duplicate empty) empty)
+(check-expect (no-pos-duplicate (list (make-pos 0 0) (make-pos 0 1) (make-pos 1 0)))
+              (list (make-pos 0 0) (make-pos 0 1) (make-pos 1 0)))
+(check-expect (no-pos-duplicate (list (make-pos 0 0) (make-pos 0 1) (make-pos 0 0))) 
+              (list (make-pos 0 1) (make-pos 0 0)))
+
+;; (define (no-pos-duplicate lop) empty) ;; stub
+
+(define (no-pos-duplicate lop)
+  (cond 
+    [(empty? lop) empty]
+    [else
+      (local
+        [
+         (define (duplicate-pos? p lop)
+           (cond
+             [(empty? lop) false]
+             [else
+              (if (and (= (pos-x p) (pos-x (first lop))) (= (pos-y p) (pos-y (first lop))))
+               true
+               (duplicate-pos? p (rest lop)))]))]
+
+      (if (duplicate-pos? (first lop) (rest lop))
+        (no-pos-duplicate (rest lop))
+        (cons (first lop) (no-pos-duplicate (rest lop)))))]))
+
+
+;; (listof Queen) Natural -> (listof Pos)
+;; consumes a list of queens and the size of the board and produce the list of position that each queen occupy
+
+(check-expect (all-queens-pos empty 4) empty)
+(check-expect (all-queens-pos  (list (make-cell (make-pos 0 2) Q)) 4) (queen-pos (make-cell (make-pos 0 2) Q) 4))
+(check-expect (all-queens-pos (list (make-cell (make-pos 0 2) Q) (make-cell (make-pos 1 0) Q)) 4) 
+              (append (queen-pos (make-cell (make-pos 0 2) Q) 4) (queen-pos (make-cell (make-pos 1 0) Q) 4)))
+
+;; (define (all-queens-pos loq sb) emtpy) ; stub
+
+(define (all-queens-pos loq sb) 
+      (cond
+        [(empty? loq) empty]
+        [else (append (queen-pos (first loq) sb) (all-queens-pos (rest loq) sb))]))
+
+;; Board -> (listof Queen)
+;; consume a boarda and produce only the queens 
+(check-expect (only-queen B0) empty)
+(check-expect (only-queen B0_1) (list (make-cell (make-pos 0 2) Q)))
+(check-expect (only-queen B0_2) (list (make-cell (make-pos 1 0) Q) (make-cell (make-pos 0 2) Q)))
+(check-expect (only-queen B0_3) (list (make-cell (make-pos 1 0) Q) (make-cell (make-pos 0 2) Q) (make-cell (make-pos 2 3) Q)))
+(check-expect (only-queen B0_S) (list (make-cell (make-pos 1 0) Q) (make-cell (make-pos 3 1) Q) (make-cell (make-pos 0 2) Q) (make-cell (make-pos 2 3) Q)))
+
+;; (define (only-queen b) empty) ; stub
+
+(define (only-queen b)
+  (local [(define (is-queen? c) (not (false? (cell-v c))))]
+    (filter is-queen? b)))
+
+;; Board (listof pos) -> Board
+;; consume a list of pos,  and filter the board them by the pos in listof pos
+;; the produced board will have less cells (only the available ones)
+
+(define P1 (list                                                ;; Quenn 0 0
+                (make-pos 0 0)                                  ;; current Quenn position
+                (make-pos 1 0) (make-pos 2 0) (make-pos 3 0)    ;; Horizontal
+                (make-pos 0 1) (make-pos 0 2) (make-pos 0 3)    ;; Vertical
+                (make-pos 1 1) (make-pos 2 2) (make-pos 3 3)		;; Diagonal br
+              ))
+
+
+(check-expect (format-board B0 P1)
+              (list
+                     (make-cell (make-pos 2 1) E) (make-cell (make-pos 3 1) E)
+                     (make-cell (make-pos 1 2) E) (make-cell (make-pos 3 2) E)
+                     (make-cell (make-pos 1 3) E) (make-cell (make-pos 2 3) E)))
+
+;; (define (format-board b lop) empty) ; stub
+
+(define (format-board b lop)
+    (local
+      [(define (same-p? c) (same-c? c lop))
+       (define (same-c? c lop) 
+         (cond 
+           [(empty? lop) true]
+           [else 
+             (if 
+               (and (= (pos-x (first lop)) (pos-x (cell-pos c))) (= (pos-y (first lop)) (pos-y (cell-pos c))))
+               false
+               (same-c? c (rest lop)))]))]
+    (filter same-p? b)))
+
+;; Queen Natural -> (listof pos) 
+;; consume a queen and the size of the board and produe the list of position that she occupy
 ;; !!! try to call this function only on the last inserted queen
 ;; ASSUME: The cell contains a queen
 
@@ -337,8 +448,17 @@
                 (make-pos 1 1) (make-pos 2 2) (make-pos 3 3)		;; Diagonal br
               ))
 
+(check-expect (queen-pos (make-cell (make-pos 0 2) Q) 4)
+              (list 
+                (make-pos 0 2)                                  ;; current Quenn position
+                (make-pos 1 2) (make-pos 2 2) (make-pos 3 2)    ;; Horizontal
+                (make-pos 0 3) (make-pos 0 1) (make-pos 0 0)    ;; Vertical
+                (make-pos 1 3)  		                            ;; Diagonal br
+                (make-pos 1 1) (make-pos 2 0) 		              ;; Diagonal tr
+              ))
 
-;; (define (queen-pos q) empty)  ; stub
+
+;; (define (queen-pos q sb) empty)  ; stub
 
 (define (queen-pos q sb) (append (list (make-pos (get-x q) (get-y q))) (queen-h-lr q sb) (queen-h-rl q) (queen-v-tb q sb) (queen-v-bt q) (queen-d-br q sb) (queen-d-bl q sb) (queen-d-tl q) (queen-d-tr q sb)))
 
